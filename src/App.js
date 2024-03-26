61b13c57cd48d63c9d3c0f21a668e3209d274488
anushreej537 <anushreej537@gmail.com>
2024-03-26 16:31:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+const App=()=>{
+  let [n,updaten] = useState(0);
+  const change=()=>{
+    updaten(n++)
+  }
+  const change1=()=>{
+    updaten(n--)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <button onClick={change}>+</button>
+    <input type='number' value={n}/>
+    <button onClick={change1}>-</button>
+    </>
+  )
 }
-
 export default App;
