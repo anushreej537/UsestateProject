3f61b358cad48a4aee4240832ca608dbcd91a151
anushreej537 <anushreej537@gmail.com>
2024-03-26 17:12:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React,{useState} from 'react'
+// import React,{useState} from 'react'
+
+// const App=()=>{
+//   let [n,updaten] = useState(0);
+//   const change=()=>{
+//     updaten(n++)
+//   }
+//   const change1=()=>{
+//     updaten(n--)
+//   }
+//   return (
+//     <>
+//     <button onClick={change}>+</button>
+//     <input type='number' value={n}/>
+//     <button onClick={change1}>-</button>
+//     </>
+//   )
+// }
+// export default App;
+
+import React,{useState} from "react";
 
 const App=()=>{
   let [n,updaten] = useState(0);
-  const change=()=>{
-    updaten(n++)
-  }
-  const change1=()=>{
-    updaten(n--)
+  const change=(e)=>{
+    if (e.target.name=='+')
+    {
+      updaten(n++);
+    }
+    else{
+      updaten(n--);
+    }
   }
   return (
     <>
-    <button onClick={change}>+</button>
-    <input type='number' value={n}/>
-    <button onClick={change1}>-</button>
+    <h2>value is:{n}</h2>
+    <button onClick={change} name='+'>Incre</button>
+    <button onClick={change} name="-">Decre</button>
     </>
   )
 }
